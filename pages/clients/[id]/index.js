6795e89b6565ebf0f5db53d54dce40a1dd0aff90
Lukorito Wabomba <bonfacewabomba@gmail.com>
2023-02-14 13:28:48 +0300

    add dynamic routing

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,9 +4,22 @@ function ClientProjectPage() {
   const router = useRouter();
   console.log({ query: router.query });
 
+  function loadProjectHandler() {
+    // router.replace(url) does not allow navigating back
+    // router.push("/clients/max/projectA");
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: {
+        id: "max",
+        clientprojectid: "projectA",
+      },
+    });
+  }
+
   return (
     <div>
       <h1>Projects of a selected client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
